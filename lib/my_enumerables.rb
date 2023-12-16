0e427366d496704c0d141d2dfa9db2445a2d58b5
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2023-12-16 07:33:29 -0800

    Complete my_each_with_index, my_select, my_all?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,28 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    self.my_each do |element|
+      yield(element, index)
+      index += 1
+    end
+  end
+
+  def my_select
+    result = []
+    self.my_each do |element|
+      result << element if yield(element)
+    end
+    result
+  end
+
+  def my_all?
+    self.my_each do |element|
+      bool = yield(element)
+      return false if bool == false
+    end
+    return true
+  end
 end
 
 # You will first have to define my_each
